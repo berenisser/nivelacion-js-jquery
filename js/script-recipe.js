330a70949ae8e27a7dc7817e9272f2d94afc4089
berenisser <berenisser@gmail.com>
2017-07-10 19:01:35 -0400

    Etapa 8: Utilizando el evento click() se agrega y quita la clase MAKE

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -3,4 +3,30 @@ $(document).ready( function(){
 	//asi que pongo el console.log en esta seccion que ejecuta funciones cuando el documento recipe.html  esta "ready"
 	console.log("Mi documento esta listo");
 
+	/*Etapa 8: se asignan funciones que se ejecuten cuando el usuario hace click en las
+	pestañas "Recipe" o "Make it", ambas funciones son equivalente a:
+	$(".js-show-recipe").click(function(){
+
+	})*/
+
+	$(".js-show-recipe").click(function(){
+		console.log("Recipe");
+       $(".page.recipe").removeClass("make");
+	})
+
+	$(".js-show-make").click(function(){
+		console.log("Make it");
+       $(".page.recipe").addClass("make");
+	})
+	/*
+	$(".btns-group").on("click", $(".js-show-recipe"), function(e) {
+		console.log("Recipe");
+       $(".page.recipe").removeClass("make");
+    });
+
+	$(".btns-group").on("click", $(".js-show-make"), function(e) {
+		console.log("Make it");
+       $(".page.recipe").addClass("make");
+    });*/
+
 });
